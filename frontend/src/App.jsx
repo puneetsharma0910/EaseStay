99aa2f9f1c4ba8378778221703833b045b95b87d
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-05-17 10:53:07 +0530

    Refactor frontend components and context for improved structure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,49 @@
-import React from "react";
-import Navbar from "./components/Navbar.jsx";
-import { Route, Routes, useLocation } from "react-router-dom";
-// import Hero from "./components/Hero.jsx";
-import Home from "./Pages/Home.jsx";
-import Footer from "./components/Footer.jsx";
-import Rooms from "./Pages/Rooms.jsx";
-import RoomDetails from "./Pages/RoomDetails.jsx";
-import MyBookings from "./Pages/MyBookings.jsx";
-import HotelReg from "./components/HotelReg.jsx";
-import Layout from "./Pages/hotelOwner/Layout.jsx";
-
-import AddRoom from "./Pages/hotelOwner/AddRoom.jsx";
-import Dashboard from "./Pages/hotelOwner/Dashboard.jsx";
-import ListRoom from "./Pages/hotelOwner/ListRoom.jsx";
+import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Layout from './pages/hotelOwner/Layout'
+import Dashboard from './pages/hotelOwner/Dashboard'
+import AddRoom from './pages/hotelOwner/AddRoom'
+import ListRoom from './pages/hotelOwner/ListRoom'
+import HotelReg from './components/HotelReg'
+import { useAppContext } from './context/AppContext'
+import { Toaster } from 'react-hot-toast'
+import AllRooms from './pages/Rooms'
+import RoomDetails from './pages/RoomDetails'
+import Footer from './components/Footer'
+import MyBookings from './pages/MyBookings'
+import Loader from './components/Loader'
 
 const App = () => {
-  const isOpenOwner = useLocation().pathname.includes("owner");
-  return (
-    <div>
-      {!isOpenOwner && <Navbar />}
-      
-      <div className="min-h-[70vh]">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rooms" element={<Rooms />} />
-          <Route path="/rooms/:id" element={<RoomDetails />} />
-         
-          <Route path="/my-bookings" element={<MyBookings />} />
-          <Route path="/owner" element={<Layout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="add-room" element={<AddRoom />} />
-            <Route path="list-room" element={<ListRoom />} />
-          </Route>
-        </Routes>
-      </div>
 
+  // Check Is Route Starts With Owner
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
+  const { showHotelReg } = useAppContext();
+
+  return (
+    <div className='font-inter'>
+      <Toaster />
+      {!isOwnerPath && <Navbar />}
+      {showHotelReg && <HotelReg />}
+      <div className='min-h-[70vh]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/rooms' element={<AllRooms />} />
+        <Route path='/rooms/:id' element={<RoomDetails />} />
+        <Route path='my-bookings' element={<MyBookings />} />
+        < Route path="/loader/:nextUrl" element={<Loader />} />
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-room" element={<AddRoom />} />
+          <Route path="list-room" element={<ListRoom />} />
+        </Route>
+      </Routes>
+      </div>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- frontend/src/Pages/hotelOwner/Layout.jsx
+++ frontend/src/Pages/hotelOwner/Layout.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../../components/hotelOwner/Navbar'
 import Sidebar from '../../components/hotelOwner/Sidebar'
 import { Outlet } from 'react-router-dom'
+import { useAppContext } from '../../context/AppContext'
 
 const Layout = () => {
-  return (
-    <div className='flex flex-col h-screen'>
-      <Navbar/>
-      <div className='flex h-full'>
-        <Sidebar/>
-        <div className='flex-1 p-4 pt-10 md:px-10 h-full'>
-          
-           
-            <Outlet/>
 
+    const { isOwner, navigate } = useAppContext()
 
+    useEffect(() => {
+        if (!isOwner) {
+            navigate('/')
+        }
+    }, [isOwner])
+
+    return (
+        <div className='flex flex-col h-screen'>
+            <Navbar />
+            <div className='flex h-full'>
+                <Sidebar />
+                <div className='flex-1 p-4 pt-10 md:px-10 h-full'>
+                    <Outlet />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Layout
